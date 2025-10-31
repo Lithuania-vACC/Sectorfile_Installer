47d92b53f69e7c46269981a91ac9e1309fe46237
nojus <me@nojus.dev>
2025-10-31 18:42:20 +0200

    fix circular imports

--- src/ui/components/__init__.py
+++ src/ui/components/__init__.py
@@ -1,11 +1,11 @@
 """UI components."""
 from ui.components.base_dialog import BaseDialog
+from ui.components.install_dialog import InstallProgressDialog, SectorfileInstructionsDialog
 from ui.components.error_dialogs import (
     NoProfilesDialog,
     SectorfileUpdateDialog,
     SettingsRequiredDialog,
 )
-from ui.components.install_dialog import InstallProgressDialog, SectorfileInstructionsDialog
 from ui.components.settings_dialog import SettingsDialog
 from ui.components.update_dialogs import MandatoryUpdateDialog, UpdateAvailableDialog
 
